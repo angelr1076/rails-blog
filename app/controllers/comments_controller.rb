9203f00bc9524021ca6dbe0325162d22be19994d
Angel Rodriguez <angelr1076@gmail.com>
2018-09-14 15:08:17 -0400

    added comments access

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,56 @@
 class CommentsController < ApplicationController
+
+  def index
+    @comments = Comment.all 
+  end
+
+  def create
+    @comment = comment.create(post_params)
+    @comment.user_id = session[:user_id]
+    if @comment.save
+      flash[:notice] = "commment created."
+      redirect_to '/posts'
+    else
+      flash[:error] = "Error creating comment."
+      render '/posts/new'
+    end
+  end
+
+  def new
+    @comment = Comment.new(post_id: params[:post_id])
+  end
+
+  def show 
+    @comment = Comment.find(params[:id])
+  end
+
+  def update
+    @comment = Comment.find(params[:id])
+    @comment.update(comment_params)
+    flash[:notice] = "Comment updated."
+    redirect_to '/posts'
+  end
+
+  def edit
+    @comment = Comment.find(params[:id])
+  end
+
+  def destroy
+    Comment.find(params[:id]).destroy
+    flash[:notice] = "Comment deleted."
+    redirect_to posts_path
+  end
+
+  def check_auth
+    if session[:user_id] != @post.user_id
+      flash[:notice] = "You can't edit this comment"
+      redirect_to posts_path
+  end
+end
+
+  private 
+  
+  def comment_params
+    params.require(:comment).permit(:user_id, :post_id)
+  end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -26,6 +26,7 @@ class PostsController < ApplicationController
  # posts#create
   def new
     @post = Post.new
+    @comment = Comment.new(post_id: params[:post_id])
   end
 
   #GET /posts/:id
